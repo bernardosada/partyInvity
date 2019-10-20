6759538771bfd7eb7434b6267daa29ccc57a3c8f
BernardoSada <BernardoSada@DESKTOP-JCDFI8T>
2019-10-20 22:18:16 +0100

    Register Form and adding a Class GuesResponse to keep the UserResponse

---
+++ partyInvity/Models/GuestResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace partyInvity.Models
+{
+    public class GuestResponse
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool? willAttend { get; set; }
+    }
+}
